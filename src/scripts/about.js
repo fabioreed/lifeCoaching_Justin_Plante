6c6390804404532447fc28fdc9c5aa96088226a9
Fabio Luiz <fabioeeereed@gmail.com>
2023-01-21 13:00:56 -0300

    add about page, footer, sections, buttons e css

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,9 +1,27 @@
-function redirectHome() {
-    const btn = document.querySelector('#home')
+import { showMenu } from './index.js'
+
+export function redirectAbout() {
+    const btn = document.querySelector('#about')
     console.log(btn)
 
     btn.addEventListener('click', () => {
+        setTimeout(() => {
+
+            location.replace('/src/pages/about.html')
+
+        }, 300)
+    })
+}
+redirectAbout()
+
+export function rendirectHome() {
+    const btn = document.querySelector('#home')
+    console.log(btn)
+    
+    btn.addEventListener('click', () => {
+        console.log('oi')
         location.replace('/')
     })
 }
-// redirectHome()
+rendirectHome()
+showMenu()
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,15 @@
-function showMenu() {
+import { redirectAbout, rendirectHome } from './about.js'
+
+export function showMenu() {
     const button = document.querySelector('.burger-menu')
     const menu = document.querySelector('.menu')
 
     button.addEventListener('click', () => {
+        console.log('clicou no menu')
         menu.classList.toggle('show-menu-mobile')
     })
 }
 showMenu()
+
+redirectAbout()
+rendirectHome()
